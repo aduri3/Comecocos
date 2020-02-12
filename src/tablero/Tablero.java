ac2ceb9621c2af002a89b4deba4492010ce9878c
usuario <usuario@usuario-OptiPlex-3050>
2020-02-12 11:07:40 +0100

    Tablero margenes

--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -1,24 +1,26 @@
 package tablero;
 
 public class Tablero {
-	for (int fil = 0; fil < tablero.length; fil++) {
-		for (int col = 0; col < tablero[fil].length; col++) {
-			tablero[fil][col]=" ";
-		}
-		
-	}
-	for (int i = 0; i < tablero.length; i++) {
-		tablero[0][i] = "-";
-		tablero[i][0] = "|";
-		tablero[i][26] = "|";
-		tablero[25][i] = "-";
-	}
-	for (int fil = 0; fil < tablero.length; fil++) {
-		for (int col = 0; col < tablero[fil].length; col++) {
-			System.out.print(tablero[fil][col]);
-		}
-		System.out.println();
-	}
 
-}
+	public void creaTablero() {
+		String[][] tablero = new String[26][27];
+		for (int fil = 0; fil < tablero.length; fil++) {
+			for (int col = 0; col < tablero[fil].length; col++) {
+				tablero[fil][col] = " ";
+			}
+
+		}
+		for (int i = 0; i < tablero.length; i++) {
+			tablero[0][i] = "-";
+			tablero[i][0] = "|";
+			tablero[i][26] = "|";
+			tablero[25][i] = "-";
+		}
+		for (int fil = 0; fil < tablero.length; fil++) {
+			for (int col = 0; col < tablero[fil].length; col++) {
+				System.out.print(tablero[fil][col]);
+			}
+			System.out.println();
+		}
+	}
 }
