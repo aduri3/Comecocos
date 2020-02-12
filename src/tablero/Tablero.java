d1cc487b5f485c656350f1acd501f55ee9b475c2
usuario <usuario@usuario-OptiPlex-3050>
2020-02-12 09:15:38 +0100

    prueba

--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -1,5 +1,24 @@
 package tablero;
 
 public class Tablero {
+	for (int fil = 0; fil < tablero.length; fil++) {
+		for (int col = 0; col < tablero[fil].length; col++) {
+			tablero[fil][col]=" ";
+		}
+		
+	}
+	for (int i = 0; i < tablero.length; i++) {
+		tablero[0][i] = "-";
+		tablero[i][0] = "|";
+		tablero[i][26] = "|";
+		tablero[25][i] = "-";
+	}
+	for (int fil = 0; fil < tablero.length; fil++) {
+		for (int col = 0; col < tablero[fil].length; col++) {
+			System.out.print(tablero[fil][col]);
+		}
+		System.out.println();
+	}
 
+}
 }
