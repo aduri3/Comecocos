4a16d975c4dbaefdeab2fa20750810ef3ca598a0
usuario <usuario@usuario-OptiPlex-3050>
2020-02-12 08:43:16 +0100

    Tablero en curso

--- src/comecocos/Tablero.java
+++ src/comecocos/Tablero.java
@@ -1,5 +1,18 @@
 package comecocos;
 
 public class Tablero {
+	private String[][] tablero;
+//	private Comecocos pacman;
+//	private Fantasma[] fantasma;
+//	private Bolas bolas;
+//	private BolaEspecial bolaEsp;
+//	private Jugador[] jugadores;
 
+	public void creaTablero() {
+		String[][] tablero = new String[50][50];
+		for (int i = 0; i < tablero.length; i++) {
+			tablero[0][i]="-";
+			tablero[i][0]="-";
+		}
+	}
 }
