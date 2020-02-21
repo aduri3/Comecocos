6dbcb95fb313242562074243b20e3a09e6afd125
usuario <usuario@usuario-OptiPlex-3050>
2020-02-21 10:16:51 +0100

    Movimiento fantasma

--- src/avatares/Fantasma.java
+++ src/avatares/Fantasma.java
@@ -1,15 +1,36 @@
 package avatares;
 
+import java.util.Scanner;
+
 //Clase del Fantasma 
 public class Fantasma {
 	private String fantasma;
 	private int puntFant;
 	private int posX;
 	private int posY;
+
 //Metodo que crea un fantasma asignandole y le asigna un nombre
 //Y los punto que va a valer
 	public Fantasma(String fant, int puntos) {
-		this.fantasma=fant;
-		this.puntFant=puntos;
+		this.fantasma = fant;
+		this.puntFant = puntos;
+	}
+
+	// Método que hace el movimiento del fantasma
+	public void movFantasma(String pos) {		
+		switch (pos) {
+		case "a":
+			posX--;
+			break;
+		case "d":
+			posX++;
+			break;
+		case "w":
+			posY++;
+			break;
+		case "s":
+			posY--;
+			break;
+		}
 	}
 }
