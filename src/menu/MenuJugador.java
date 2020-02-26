6c5691583502615fc074341b07fbd3b9dc9a9ac2
usuario <usuario@usuario-OptiPlex-3050>
2020-02-26 08:28:44 +0100

    Subida rapida

--- src/menu/MenuJugador.java
+++ src/menu/MenuJugador.java
@@ -2,4 +2,24 @@ package menu;
 
 public class MenuJugador {
 
+	// Método que da la interfaz de los movimientos
+	/**
+	 * Método que devuelve la interfaz de los posibles movimientos del jugador
+	 * 
+	 * @return 
+	 *		<ul>
+	 *			<li>W - Arriba</li>
+	 *			<li>A - Izquierda</li>
+	 *			<li>S - Abajo</li>
+	 *			<li>D - Derecha</li>
+	 *		</ul>
+	 */
+	public String getInterfazMovimientos() {
+		return "Mueva con las teclas:\n" + 
+				"W - Arriba\n" + 
+				"A - Izquierda\n" + 
+				"S - Abajo\n" + 
+				"D - Derecha";
+	}
+	
 }
