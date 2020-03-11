9b4132083b33a9e6cc6eb3509bca496593ed499d
usuario <usuario@usuario-OptiPlex-3050>
2020-03-11 08:53:29 +0100

    MenuJugador teclas

--- src/menu/MenuJugador.java
+++ src/menu/MenuJugador.java
@@ -4,21 +4,27 @@ public class MenuJugador {
 
 	// Método que da la interfaz de los movimientos
 	/**
+	 * Clase MenuJugador
+	 * 
+	 * Contiene las teclas de los movimientos
+	 * 
 	 * @version 1.0
 	 * @author Roberto Ruiz
 	 * 
+	 */
+	
+	/**
 	 * Método que devuelve la interfaz de los posibles movimientos del jugador
 	 * 
+	 * @return " -----\n" +" | W |\n" +" -----\n" + "----- ----- -----\n" + "| A | |
+	 *         S | | D |\n" + "----- ----- -----\n";
+	 * 
+	 * @version 1.0
+	 * @author Roberto Ruiz 
 	 */
 	public String getInterfazMovimientos() {
-		return "       -----\n"
-				+"       | W |\n"
-				+"       -----\n"
-				+ "-----  -----  -----\n"
-				+ "| A |  | S |  | D |\n"
-				+ "-----  -----  -----\n";
+		return "       -----\n" + "       | W |\n" + "       -----\n" + "-----  -----  -----\n"
+				+ "| A |  | S |  | D |\n" + "-----  -----  -----\n";
 	}
-	
-	
-	
+
 }
