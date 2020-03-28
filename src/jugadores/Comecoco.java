d3b678920d79782ee5f2c3851b91e1ba7061087a
XByDa <XByDa@DESKTOP-B0PQ5GD>
2020-03-28 12:27:07 +0100

    Actualizar javaDoc

--- src/jugadores/Comecoco.java
+++ src/jugadores/Comecoco.java
@@ -1,8 +1,23 @@
 package jugadores;
 
-//Clase del Comecoco
+/**
+ * Clase Comecoco
+ * 
+ * La cual continene los atributos del Comecoco y la posicion de estos
+ * 
+ * @author Dani Kuradchyk
+ * 
+ */
+
 public class Comecoco extends Jugador {
 
+	/**
+	 * Metodo constructor que inicializa el Comecoco
+	 * 
+	 * @param icono cambia dependiendo del movimiento
+	 * @param nombreJug nombre del jugador
+	 * 
+	 */
 	public Comecoco(String icono, String nombreJug) {
 		super(icono, nombreJug);
 		// TODO Auto-generated constructor stub
--- src/jugadores/Fantasma.java
+++ src/jugadores/Fantasma.java
@@ -2,11 +2,23 @@ package jugadores;
 
 import java.util.Scanner;
 
-//Clase del Fantasma 
+/**
+ * Clase Fantasma
+ * 
+ * La cual continene los atributos del fantasma y la posicion de estos
+ * 
+ * @author Dani Kuradchyk
+ * 
+ */
 public class Fantasma extends Jugador{
 
-//Metodo que crea un fantasma asignandole y le asigna un nombre
-//Y los punto que va a valer
+	/**
+	 * Metodo constructor que inicializa el Jugador
+	 * 
+	 * @param icono cambia dependiendo del movimiento
+	 * @param nombreJug nombre del jugador
+	 * 
+	 */
 	public Fantasma(String icono, String nombreJug) {
 		super(icono, nombreJug);
 		// TODO Auto-generated constructor stub
@@ -15,6 +27,11 @@ public class Fantasma extends Jugador{
 		this.nMov=calculaNmov();
 		
 	}
+	/**
+	 * Metodo que devuelve el numero de movimientos
+	 * 
+	 * @return devuelve un numero aletorio del 0 al 3
+	 */
 	private int calculaNmov() {
 		return (int) (Math.random()*4); 
 	}
